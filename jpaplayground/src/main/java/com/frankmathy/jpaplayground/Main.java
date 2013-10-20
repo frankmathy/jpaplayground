bc0282e457ca9c312933b4390e78ec0a594dba4f
Frank Mathy <frankmathy@gmail.com>
2013-10-20 17:23:19 +0200

    Created simple hibernate example

---
+++ jpaplayground/src/main/java/com/frankmathy/jpaplayground/Main.java
@@ -0,0 +1,33 @@
+package com.frankmathy.jpaplayground;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import com.frankmathy.jpaplayground.model.Person;
+
+public class Main {
+
+	private static final String PERSISTENCE_UNIT_NAME = "persistence";
+	private static EntityManagerFactory factory;
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		factory = Persistence.createEntityManagerFactory(PERSISTENCE_UNIT_NAME);
+		EntityManager em = factory.createEntityManager();
+		
+		em.getTransaction().begin();
+		
+		Person p = new Person();
+		p.setFirstName("Frank");
+		p.setLastName("Mathy");
+		em.persist(p);
+		
+		em.getTransaction().commit();
+		
+		em.close();
+	}
+
+}
